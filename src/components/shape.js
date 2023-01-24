a9fc45cdd74b89a811e6a28cd87d3be198825f50
Anshu Kumar Singh <anshukumarsingh444@gmail.com>
2023-01-24 20:49:58 +0530

    change done

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,8 +1,21 @@
 // declare class
-function Shape(color) {
-    this.color = '#333';
-    this.drawShape = function () {};
-    this.calculateArea = function () {};
+// function Shape(color) {
+//     this.color = '#333';
+//     this.drawShape = function () {};
+//     this.calculateArea = function () {};
+// }
+
+class Shape {
+    constructor() {
+        this.color = "#FF40FF";
+        this.state = () => { return 5} 
+    }
+    drawShape() {
+        return this.color;
+    }
+    calculateArea() {
+        return 1;
+    }
 }
 // export class using module.exports
 module.exports = Shape;
