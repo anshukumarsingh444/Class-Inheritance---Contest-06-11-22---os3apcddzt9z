69241b1fd6b23049be59b23c668c60bf0acc41c7
Anshu Kumar Singh <anshukumarsingh444@gmail.com>
2023-01-24 20:28:45 +0530

    code as per requirement

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
+import { Shape } from './shape.js';
 
-// declare class
+//  declare class
+function Circle() {
+    Shape.call(this);
+} 
+
+Circle.prototype.__proto__ = Shape.prototype;
+Circle.prototype.__proto__.calculateArea = function() {};
 
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,8 @@
 // declare class
-
+function Shape(color) {
+    this.color = '#333';
+    this.drawShape = function () {};
+    this.calculateArea = function () {};
+}
 // export class using module.exports
+module.exports = Shape;
